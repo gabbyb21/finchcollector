8e7ab76ae7d8b3b10cb7ddaa98df9babe75f56cd
gabbyb21 <gabbailo94@gmail.com>
2023-07-06 19:51:49 -0400

    created model and details page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+  breed = models.CharField(max_length=100)
+  habitat = models.CharField(max_length=100)
+  food = models.CharField(max_length=100)
+
+def __str__(self):
+  return self.breed
