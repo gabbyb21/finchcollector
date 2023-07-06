4fefc3f48e8acdb9349c03d8d775bcc41e999611
gabbyb21 <gabbailo94@gmail.com>
2023-07-06 11:30:24 -0400

    added about and finches page

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+finches = [
+  {'breed': 'America Goldfinch', 'habitat': 'open woodlands', 'food': 'seeds'},
+  {'breed': 'Black Rosy-Finch', 'habitat': 'tundra', 'food': 'seeds'},
+  {'breed': 'Blue Grosbeak', 'habitat': 'open woodlands', 'food': 'insects'},
+]
+
 # Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def finches_index(request):
+  return render(request, 'finches/index.html', {
+    'finches': finches
+  })
